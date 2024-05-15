5560717e4c56400b29a550e67371124b35578694
jailtoncarlos@gmail.com <jailton.paiva@lais.huol.ufrn.br>
2024-05-15 02:39:53 -0300

    Estruturando pastas do projeto

--- bootcamp_engenharia_dados/docker/apache_hop/create-directory.sh
+++ bootcamp_engenharia_dados/docker/apache_hop/create-directory.sh
@@ -8,5 +8,9 @@ mkdir -p data/server/projects/files
 mkdir -p data/web_ui/config
 mkdir -p data/web_ui/projects/metadata
 mkdir -p data/web_ui/projects/files
+mkdir -p data/web_ui/projects/datasets
+mkdir -p data/web_ui/projects/pipelines
+mkdir -p data/web_ui/projects/transforms
+mkdir -p data/web_ui/projects/workflows
 
 chmod -R 777 data/
