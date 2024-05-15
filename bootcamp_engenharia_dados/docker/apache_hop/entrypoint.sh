5e263ca6d858029595e226a2641bb82035e14cc8
jailtoncarlos@gmail.com <jailton.paiva@lais.huol.ufrn.br>
2024-05-15 05:39:50 -0300

    containers hop-server e hop-web configurados para usar o mesmo Dockerfile. Crido usuário:grupo hopuser:hopgroup com o mesmo UID:GID do usuário da máquina local

--- bootcamp_engenharia_dados/docker/apache_hop/entrypoint.sh
+++ bootcamp_engenharia_dados/docker/apache_hop/entrypoint.sh
@@ -3,5 +3,8 @@
 # Ajusta as permissões dos diretórios
 chown -R hopuser:hopgroup /hop
 
+# Ajusta as permissões dos diretórios antes de iniciar o servidor
+chown -R hopuser:hopgroup /usr/local/tomcat
+
 # Executa o comando como o usuário especificado
-exec gosu hopuser:hopgroup "$@"
+exec "$@"
